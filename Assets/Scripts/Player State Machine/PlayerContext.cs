057ff1257cc554c9fa87b47c3aaf402428ae312d
Marcus Ureta <silvergreat0@gmail.com>
2026-01-29 03:01:18 +0800

    basic movement system up

--- Assets/Scripts/Player State Machine/PlayerContext.cs
+++ Assets/Scripts/Player State Machine/PlayerContext.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -16,18 +18,23 @@ public class PlayerContext : MonoBehaviour
     #endregion
 
     private const float MAX_FALL_SPEED = 18;
+    private const float JUMP_APEX_THRESHOLD = 0.185f; // temporary implementation if apex hanging
+    private const float SPEED_BOOST_SLIDE_TIMER = 2f;
 
     [Header("Movement Variables")]
     [SerializeField] private int _playerSpeed = 11;
-    [SerializeField] private float jumpForce = 7f;
-    [SerializeField] private float fallMultiplier = 2.5f;
-    [SerializeField] private float lowJumpMultiplier = 4f;
+    [SerializeField] private float _jumpForce = 7f;
+    [SerializeField] private float _slideBoost = 2.65f;
+    [SerializeField] private float _fallMultiplier = 2.5f;
+    [SerializeField] private float _lowJumpMultiplier = 4f;
+    private bool _startApexTimer;
+    private float _apexCounter;
     private float _bonusSpeed;
     private bool _grounded; // TEMPORARY
 
     [Header("Sensitivity")]
-    [SerializeField] private float _rotateSpeed_X = 90f;
-    [SerializeField] private float _rotateSpeed_Y = 110f;
+    [SerializeField] private float _rotateSpeed_X = 60f;
+    [SerializeField] private float _rotateSpeed_Y = 65f;
     [SerializeField] Transform _cameraPoint;
     private float _pitch;
     private float minPitch = -80f;
@@ -35,7 +42,6 @@ public class PlayerContext : MonoBehaviour
 
     [Header("Miscellaneous")]
     [SerializeField] private GameObject _temporaryObject;
-
     private Rigidbody _rb;
     Vector2 _moveDir = Vector2.zero;
     Vector2 _lookDir = Vector2.zero;
@@ -76,42 +82,48 @@ public class PlayerContext : MonoBehaviour
 
         MouseLook();
 
-        if (_jumpAction.WasPressedThisFrame() && _grounded)
-        {
-            Jump();
-        }
-
         if (_attackAction.WasPressedThisFrame())
         {
             GameObject temp = Instantiate(_temporaryObject);
 
             temp.transform.position = this.transform.position;
         }
+
+        if (_jumpAction.WasPressedThisFrame() && _grounded) Jump();
+
+        if (_slideAction.WasPressedThisFrame()) Sliding();
+        if (_slideAction.WasReleasedThisFrame()) CancelSlide();
+
+        if (_startApexTimer) _apexCounter += Time.deltaTime;
     }
 
     private void FixedUpdate()
     {
         Vector3 player_movement = (transform.forward * _moveDir.y + transform.right * _moveDir.x);
 
-        player_movement *= _playerSpeed;
 
-        _rb.linearVelocity = new Vector3(player_movement.x, _rb.linearVelocity.y, player_movement.z);
+        _bonusSpeed = Math.Clamp(_bonusSpeed, 0, 25);
+        player_movement *= _playerSpeed + _bonusSpeed;
 
         ApplyBetterGravity();
+
+        _rb.linearVelocity = new Vector3(player_movement.x, _rb.linearVelocity.y, player_movement.z);
     }
 
 
     void ApplyBetterGravity()
     {
-        if (_rb.linearVelocity.y < 0)
+        if (_rb.linearVelocity.y < 0 || (_startApexTimer && _apexCounter >= JUMP_APEX_THRESHOLD))
         {
-            // Currently Falling
-            _rb.linearVelocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.fixedDeltaTime;
+            // Once falling, increase player's gravity
+            ResetApex();
+            _rb.linearVelocity += Vector3.up * Physics.gravity.y * (_fallMultiplier - 1) * Time.fixedDeltaTime;
         }
         else if (_rb.linearVelocity.y > 0 && !_jumpAction.IsPressed())
         {
             // Jump released early, fall faster
-            _rb.linearVelocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.fixedDeltaTime;
+            ResetApex();
+            _rb.linearVelocity += Vector3.up * Physics.gravity.y * (_lowJumpMultiplier - 1) * Time.fixedDeltaTime;
         }
 
         // Clamp fall speed
@@ -124,8 +136,9 @@ public class PlayerContext : MonoBehaviour
     void Jump()
     {
         _rb.linearVelocity = new Vector3(_rb.linearVelocity.x, 0f, _rb.linearVelocity.z);
-        _rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-        _bonusSpeed += 2;
+        _startApexTimer = true;
+        _rb.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+        _bonusSpeed += 1;
     }
 
     void MouseLook()
@@ -141,15 +154,37 @@ public class PlayerContext : MonoBehaviour
         _cameraPoint.localRotation = Quaternion.Euler(_pitch, 0f, 0f);
     }
 
-    // TEMPORARY GROUND COLLISION CHECKS
-    private void OnCollisionEnter(Collision collision)
+    void ResetApex()
     {
-        _bonusSpeed = 0;
-        _grounded = true;
+        _startApexTimer = false;
+        _bonusSpeed -= 1;
+        _apexCounter = 0;
     }
 
+    void Sliding()
+    {
+        _bonusSpeed += _slideBoost;
+        StartCoroutine(SlideCooldown());
+        this.transform.localScale = new Vector3(1, 0.5f, 1);
+    }
+    
+    void CancelSlide()
+    {
+        this.transform.localScale = new Vector3(1, 1, 1);
+    }
+
+
+    private IEnumerator SlideCooldown()
+    {
+        // yes another temporary function lol i dont want to exert that much brainpower at 3am
+        yield return new WaitForSeconds(SPEED_BOOST_SLIDE_TIMER);
+        _bonusSpeed -= _slideBoost;
+    }
+
+    // ---   TEMPORARY GROUND COLLISION CHECKS    --- \\
     private void OnCollisionStay(Collision collision)
     {
+        ResetApex();
         _grounded = true;
     }
 
