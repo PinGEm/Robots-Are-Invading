334afdc33dea2c24cd2fb4113560b183e564812c
notrllyxyion <mikelreuelramos69@gmail.com>
2026-04-26 00:47:16 +0800

    Options/Controls UI
    Continued Editing the Options UI and Started designing the Controls UI

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,7 +4,9 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private GameObject _optionsMenu;
+    
     [SerializeField] private GameObject _mainMenu;
+    [SerializeField] private GameObject _controlsMenu;
 
     public void EnterGame()
     {
@@ -21,6 +23,18 @@ public class MainMenu : MonoBehaviour
     {
         _mainMenu.SetActive(true);
         _optionsMenu.SetActive(false);
+    }
+
+    public void ControlOptions()
+    {
+         _controlsMenu.SetActive(true);
+        _optionsMenu.SetActive(false);
+    }
+    
+    public void ExitControlOptions()
+    {
+        _controlsMenu.SetActive(false);
+        _optionsMenu.SetActive(true);
     }
 
     public void ExitGame()
