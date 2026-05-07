12db2883d048208667fa727905763c1f9dcff66d
Marcus Ureta <silvergreat0@gmail.com>
2026-05-07 21:55:14 +0800

    Level Six Baked Navmeshes

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -10,7 +10,7 @@ public class MainMenu : MonoBehaviour
 
     public void EnterGame()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(2);
     }
 
     public void OpenOptions()
